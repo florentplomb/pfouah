436893119b3d6ee1843a31887c9730e2edff2362
Laurent Lienher <laurent.lienher@gmail.com>
2015-04-16 15:55:29 +0200

    test modal

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,84 +1,63 @@
 'use strict';
 
 angular.module('transmedApp')
-  .controller('MainCtrl', function ($scope, $http, socket, $log, ApiService) {
 
-  // --- Code exemple 
-    $scope.awesomeThings = [];
+.controller('MainCtrl', function ($scope, $modal, $log) {
+  $scope.items = ['item1', 'item2', 'item3'];
 
-    /*$http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
-    });*/
+  $scope.open = function (size) {
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
+    var modalInstance = $modal.open({
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
       }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
     });
 
-  // --- Mon code
-   /* $scope.getStats = function(){
-      getScores().then(getUsers).then(getGames).then(function (data){
-        // use scores length
-        // gameId + userId -> gameName + userName
-        // send everything in $score.stats
-      });
-    }
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+})
 
-    getScores = function(){
-      return 
-        $http.get('/api/scores').success(function(scores){
-        $scope.scores = scores;
-        socket.syncUpdates('scores', $scope.scores);
-      }).error(function(errorCallback){
-          $scope.message = 'Une erreur s\' produite : ' + errorCallback;
-      });
-    }
 
-    getUsers = function(){
-      $http.get('api/users').success(function(users){
-        $scope.users = users;
-        socket.syncUpdates('users', $scope.scores);
-      }).error(function(errorCallback){
-        $scope.message = 'Une erreur s\' produite : ' + errorCallback;
-      })
-    }
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
 
-    getGames = function(){
-      $http.get('api/games').success(function(games){
-        $scope.games = games;
-        socket.syncUpdates('games', $scope.games);
-      }).error(function(errorCallback){
-        $scope.message = 'Une erreur s\' produite : ' + errorCallback;
-      })
-    }*/
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+})
 
 
 
 
 
-    ApiService.getThings(
-      function(data){
-        $scope.awesomeThings = data;
-        socket.syncUpdates('thing', $scope.awesomeThings);
-      }, 
-      function(error){
-        $scope.message = error;
-      }
-    );
 
-  })
+
+
+
+
+
+.controller('MainCtrl', function ($scope, $http, socket, $log) {
+
+})
 
 .factory('ApiService', function($http){
     return{
