d6ac7b6bce14fa4a933897c4e2971e45154a5e7d
Laurent Lienher <laurent.lienher@gmail.com>
2015-04-17 13:14:59 +0200

    change on ranking controller

--- client/app/ranking/ranking.controller.js
+++ client/app/ranking/ranking.controller.js
@@ -2,14 +2,14 @@
 
 angular.module('transmedApp')
 	.controller('RankingCtrl', function ($scope, $http, socket, $log, RankingService){
-		$scope.fetch = function(){
-			RankingService.getUsers(
-				function(data){
-					$scope.res = data;
-				},
-				function(error){
-					$scope.error = error;
-				}
-			);
-		}
+
+		RankingService.getUsers(
+			function(data){
+				$scope.res = data;
+			},
+			function(error){
+				$scope.error = error;
+			}
+		);
+		
 	});
