1b7a705d43d70407c15f6a2328e19cb8dc503289
Laurent Lienher <laurent.lienher@gmail.com>
2015-04-19 14:04:07 +0200

    add static datas json files and ranking tabs

--- client/app/ranking/ranking.controller.js
+++ client/app/ranking/ranking.controller.js
@@ -1,15 +1,35 @@
 'use strict';
 
 angular.module('transmedApp')
-	.controller('RankingCtrl', function ($scope, $http, socket, $log, RankingService){
+	.controller('RankingCtrl', function ($scope, $http, socket, $log, StaticService){
 
-		RankingService.getUsers(
+		var datas = [];
+
+		StaticService.getUsers(
 			function(data){
-				$scope.res = data;
+				$scope.datas = data;
+				datas = data;
 			},
 			function(error){
 				$scope.error = error;
 			}
 		);
+
+		$scope.tabs = [
+			{'title' : 'Scores totaux', 'id': '1'},
+			{'title' : 'Best Scores : cummulés', 'id': '2'},
+			{'title' : 'Best Scores : Wash', 'id': '3'},
+			{'title' : 'Best Scores : trash', 'id': '4'},
+			{'title' : 'Best Scores : flash', 'id': '5'}
+		];
+
+		$scope.content = [];
+
+		$scope.activeTab = $scope.tabs[0].id;
+
+		$scope.showTab = function (currentTab){
+			$scope.activeTab = currentTab;
+			//$log.debug(currentTab);
+		};
 		
 	});
--- client/app/ranking/ranking.service.js
+++ client/app/ranking/ranking.service.js
@@ -1,4 +1,6 @@
 'use strict';
+// https://zkillboard.com/api/kills/characterID/91706280/
+// http://pfouah2015.herokuapp.com/api/users
 
 angular.module('transmedApp')
 	.factory('RankingService', function($http){
@@ -7,6 +9,21 @@ angular.module('transmedApp')
 				$http({
 					method: 'GET',
 					url: 'http://pfouah2015.herokuapp.com/api/users'
+				}).success(function (data){
+					callback(data);
+				}).error(function (data){
+					errorCallback(data);
+				});
+			}
+		}
+	})
+
+	.factory('StaticService', function($http){
+		return{
+			getUsers : function(callback, errorCallback){
+				$http({
+					method: 'GET',
+					url: 'assets/json/users.json'
 				}).success(function (data){
 					callback(data);
 				}).error(function (data){
