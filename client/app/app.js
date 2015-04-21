d6ed08d31ad12e6095d72fd4aa24af15e14530ce
LL0892 <laurent.lienher@gmail.com>
2015-04-21 11:12:06 +0200

    add plugin in angular module and html

--- client/app/app.js
+++ client/app/app.js
@@ -7,7 +7,8 @@ angular.module('transmedApp', [
   'btford.socket-io',
   'ui.router',
   'ui.bootstrap',
-  'ngDialog'
+  'ngDialog',
+  'LocalStorageModule'
 ])
 
   .constant('TwitterUrl', 'https://api.twitter.com/1.1/')
