857edd275c339bf8098a898dd3e49fcd865b3d86
florentplomb <plombf@gmail.com>
2015-04-21 13:22:08 +0200

    complet auth and role

--- server/auth/auth.service.js
+++ server/auth/auth.service.js
@@ -7,7 +7,9 @@ var jwt = require('jsonwebtoken');
 var expressJwt = require('express-jwt');
 var compose = require('composable-middleware');
 var User = require('../api/user/user.model');
-var validateJwt = expressJwt({ secret: config.secrets.session });
+var validateJwt = expressJwt({
+  secret: config.secrets.session
+});
 var _ = require('underscore');
 
 /**
@@ -17,50 +19,32 @@ var _ = require('underscore');
 
 function isAuthenticated() {
 
-   /////////////// Auth fullstack ///////////////////
   return compose()
-    // Validate jwt
 
-    .use(function(req, res, next) {
+  .use(function(req, res, next) {
 
-          var userId = req.headers['x-user-id'];
+    if (!req.headers['salt']) return res.status(400).json({
+      message: 'header wrong'
+    }).end();
+    var userSalt = req.headers['salt'];
 
-    if (userId !== undefined) {
-      User.findById(userId, function (err, user) {
-        if (err || user === null) {
-          res.status(401).end();
-        }
-        else if (user.roles.length > 0) {
-          req.user = user;
-          next();
-        }
-        else {
-          res.status(403).end();
+    User
+      .find()
+      .and({
+        salt: userSalt
+      })
+      .exec(function(err, userFound) {
+        if (userFound.length === 0) {
+          res.status(422).json({
+            message: 'Not authorized'
+          }).end();
+        }else{
+            next();
+
         }
       });
-    }
-    else {
-      res.status(401).end();
-    }
+  });
 
-
-      // // allow access_token to be passed through query parameter as well
-      // if(req.query && req.query.hasOwnProperty('access_token')) {
-      //   console.log("ici");
-      //    req.headers.authorization = 'Bearer ' + req.query.access_token;
-      //  }
-      //  validateJwt(req, res, next);
-    })
-   // Attach user to request
-    .use(function(req, res, next) {
-      User.findById(req.user._id, function (err, user) {
-        if (err) return next(err);
-        if (!user) return res.send(401);
-
-        req.user = user;
-        next();
-      });
-    });
 }
 
 /**
@@ -68,62 +52,45 @@ function isAuthenticated() {
  */
 function hasRole(roleRequired) {
 
-  if (!roleRequired) throw new Error('Required role needs to be set');
+    return compose()
 
-   return compose()
-   .use(function(req, res, next) {
+  .use(function(req, res, next) {
 
-    var userId = req.headers['x-user-id'];
+    if (!req.headers['salt']) return res.status(400).json({
+      message: 'header wrong'
+    }).end();
+    var userSalt = req.headers['salt'];
 
-    if (userId !== undefined) {
-      User.findById(userId, function (err, user) {
-        if (err || user === null) {
-          res.status(401).end();
-        }
-        else if (user.roles === roleRequired) {
-          req.user = user;
-          next();
-        }
-        else {
-          res.json("votre role ne permet pas d'acceder à cette resource").end();
+    User
+      .find()
+      .and({
+        salt: userSalt
+      })
+      .and({roles : roleRequired })
+      .exec(function(err, userFound) {
+        if (userFound.length === 0) {
+          res.status(422).json({
+            message: 'Not authorized'
+          }).end();
+        }else{
+            next();
+
         }
       });
-    }
-    else {
-      res.status(401).end();
-    }
+  });
 
-
-      // // allow access_token to be passed through query parameter as well
-      // if(req.query && req.query.hasOwnProperty('access_token')) {
-      //   console.log("ici");
-      //    req.headers.authorization = 'Bearer ' + req.query.access_token;
-      //  }
-      //  validateJwt(req, res, next);
-    })
-  //   .use(isAuthenticated())
-  //   .use(function meetsRequirements(req, res, next) {
-  //     if (config.userRoles.indexOf(req.user.role) >= config.userRoles.indexOf(roleRequired)) {
-  //       next();
-  //     }
-  //     else {
-  //       res.send(403);
-  //     }
-  //   });
 }
 
-/**
- * Returns a jwt token signed by the app secret
- */
-function signToken(id) {
-  return jwt.sign({ _id: id }, config.secrets.session, { expiresInMinutes: 60*5 });
-}
+
+
 
 /**
  * Set token cookie directly for oAuth strategies
  */
 function setTokenCookie(req, res) {
-  if (!req.user) return res.json(404, { message: 'Something went wrong, please try again.'});
+  if (!req.user) return res.json(404, {
+    message: 'Something went wrong, please try again.'
+  });
   var token = signToken(req.user._id, req.user.role);
   res.cookie('token', JSON.stringify(token));
   res.redirect('/');
@@ -131,5 +98,5 @@ function setTokenCookie(req, res) {
 
 exports.isAuthenticated = isAuthenticated;
 exports.hasRole = hasRole;
-exports.signToken = signToken;
+
 exports.setTokenCookie = setTokenCookie;
