ffd5c47a77128a66a99e10c1858f2ccb7989f42f
LL0892 <laurent.lienher@gmail.com>
2015-04-22 12:08:47 +0200

    change on html modals

--- client/app/main/modal/modal.controller.js
+++ client/app/main/modal/modal.controller.js
@@ -7,7 +7,7 @@ angular.module('transmedApp')
   	
   	$scope.openSecond = function (dataVideo) {
     	ngDialog.close();
-    	
+
 	   	ngDialog.open({
 		    // url from index.html at the root
 		    template: 'app/main/modal/modal2.html',
@@ -28,13 +28,28 @@ angular.module('transmedApp')
 // Second modal configs & actions
  .controller('SecondPopupCtrl', function ($scope, $log, ngDialog) {
 
+ 	$scope.feedback = '';
+
   	$scope.closeSecond = function () {
     	ngDialog.close();
   	};
 
   	$scope.submitForm = function (answer){
+
   		$log.debug(answer);
-  		$scope.feedback = 'answer succesfully sent !';
+  		
+  		// answer control
+  		if (answer.length <= 2 || answer.length >= 140) {
+  			answer = undefined;
+  		};
+
+  		if (answer != undefined) {
+  			$log.debug('if');
+  			$scope.feedback = 'Réponse envoyée avec succès !';
+  		}else{
+  			$log.debug('else');
+  			$scope.feedback = 'Il n\'y a rien à envoyer.';
+  		}
   	}
 
 });
