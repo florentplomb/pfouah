50abc41647a052a8cb5fa1207a2401045f6791d4
LL0892 <laurent.lienher@gmail.com>
2015-04-22 11:17:19 +0200

    update json files + add data persistance to modal controllers

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,20 +2,29 @@
 
 angular.module('transmedApp')
 
-.controller('MainCtrl', function ($scope, ngDialog) {
+.controller('MainCtrl', function ($scope, $log, ngDialog) {
+
+  $scope.videos = [
+    {'title' : 'video 1', 'question' : 'question 1'},
+    {'title' : 'video 2', 'question' : 'question 2'}
+  ];
 
   // initial action button, open the first modal
-  $scope.openDefault = function () {
+  $scope.openDefault = function (dataVideo) {
+
     ngDialog.open({
       // url from index.html at the root
       template: 'app/main/modal/modal1.html',
       plain: false,
       controller: 'FirstPopupCtrl',
       className: 'ngdialog-theme-default',
+      data: dataVideo,
       closeByEscape: false,
       closeByDocument: false,
       showClose: false,
       cache: false
     });
+
   };
+
 });
--- client/app/main/modal/modal.controller.js
+++ client/app/main/modal/modal.controller.js
@@ -4,25 +4,30 @@ angular.module('transmedApp')
 
 // First modal configs & actions
 .controller('FirstPopupCtrl', function ($scope, $log, ngDialog) {
-  	$scope.openSecond = function () {
+  	
+  	$scope.openSecond = function (dataVideo) {
     	ngDialog.close();
-    	$log.debug('second modal');
-	   		ngDialog.open({
+    	
+	   	ngDialog.open({
 		    // url from index.html at the root
 		    template: 'app/main/modal/modal2.html',
 		    plain: false,
 		    controller: 'SecondPopupCtrl',
 		    className: 'ngdialog-theme-default',
+		    data: dataVideo,
 		    closeByEscape: false,
 		    closeByDocument: false,
 		    showClose: false,
 		    cache: false
 		});
+
   	};
+
 })
 
 // Second modal configs & actions
  .controller('SecondPopupCtrl', function ($scope, $log, ngDialog) {
+
   	$scope.closeSecond = function () {
     	ngDialog.close();
   	};
@@ -31,4 +36,5 @@ angular.module('transmedApp')
   		$log.debug(answer);
   		$scope.feedback = 'answer succesfully sent !';
   	}
+
 });
