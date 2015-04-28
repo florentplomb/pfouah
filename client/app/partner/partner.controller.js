438bd84e7138919a0962e9b1a49e52217a1d1be4
LL0892 <laurent.lienher@gmail.com>
2015-04-28 11:27:52 +0200

    add partners' logo, change on html

--- client/app/partner/partner.controller.js
+++ client/app/partner/partner.controller.js
@@ -3,9 +3,9 @@
  angular.module('transmedApp')
  	.controller('PartnerCtrl', function ($scope, $log){
  		$scope.partners = [
- 			{"name" : "Race For Water", "twitter" : "https://twitter.com/RaceForWater", "facebook" : "https://www.facebook.com/raceforwaterfoundation", "site" : "http://www.raceforwater.com/", "sentence" : ""},
- 			{"name" : "TrinkWasser", "twitter" : "", "facebook" : "https://de-de.facebook.com/SVGWSSIGE", "site" : "http://trinkwasser.svgw.ch/", "sentence" : ""},
- 			{"name" : "ASL", "twitter" : "", "facebook" : "https://www.facebook.com/asl.leman.1", "site" : "http://asleman.org/fr/", "sentence" : ""}
+ 			{"name" : "Race For Water", "twitter" : "https://twitter.com/RaceForWater", "facebook" : "https://www.facebook.com/raceforwaterfoundation", "site" : "http://www.raceforwater.com/", "logo" : "raceforwater_logo.png"},
+ 			{"name" : "TrinkWasser", "twitter" : "", "facebook" : "https://de-de.facebook.com/SVGWSSIGE", "site" : "http://trinkwasser.svgw.ch/", "logo" : "Logo_Trinkwasser_cmyk.png"},
+ 			{"name" : "ASL", "twitter" : "", "facebook" : "https://www.facebook.com/asl.leman.1", "site" : "http://asleman.org/fr/", "logo" : "asl_logo.png", "style" : "asl"}
  		];
 
  		$log.debug($scope.partners);
