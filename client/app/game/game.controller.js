5ac6d5031d9a28f397b193ac087d95bb70b1c2c2
LL0892 <laurent.lienher@gmail.com>
2015-04-30 15:22:33 +0200

    add fonts, text with right fonts

--- client/app/game/game.controller.js
+++ client/app/game/game.controller.js
@@ -4,8 +4,8 @@ angular.module('transmedApp')
 
 .controller('GameCtrl', function ($scope, $log) {
 	$scope.games = [
-	{"title" : "Wash", "description" : "", "logo" : "", "partner" : "Partner 1", "logoPartner" : ""},
-	{"title" : "Trash", "description" : "", "logo" : "", "partner" : "Partner 1", "logoPartner" : ""},
-	{"title" : "Splash", "description" : "", "logo" : "", "partner" : "Partner 1", "logoPartner" : ""}
+	{"title" : "Wash", "description" : "« Même les particules les plus minuscules nous intéressent. »", "logo" : "wash2.png", "partner" : "Oceaneye", "url" : "http://www.oceaneye.eu/"},
+	{"title" : "Trash", "description" : "« Ceci qui commence à la caisse, se termine souvent au milieu de l’ocean. »", "logo" : "trash2.png", "partner" : "Race for Water", "url" : "http://www.raceforwater.com/"},
+	{"title" : "Flash", "description" : "« Une meilleure connaissance permet de mieux comprendre. »", "logo" : "flash2.png", "partner" : "Association Développement 21", "url" : "http://www.eau21.ch"}
 	];
 1});
