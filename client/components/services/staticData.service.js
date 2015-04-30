df9fd7c4fdba8f8f863b896ad7605a80452bc6dc
LL0892 <laurent.lienher@gmail.com>
2015-04-30 11:59:04 +0200

    add datas for 3nd video, add 3nd feed

--- client/components/services/staticData.service.js
+++ client/components/services/staticData.service.js
@@ -3,8 +3,9 @@
 angular.module('transmedApp')
 	.factory('DataService', function($http){
 		var videos = [
-		    {'title' : 'Je suis en manque', 'question' : 'Et toi, c’est quoi ta dope?', 'hashtag' : '#pfouahQ1'},
-		    {'title' : 'Le rencard immaculé', 'question' : 'Deux jours sans douche, ça te ferait quoi?', 'hashtag' : '#pfouahQ2'}
+		    {'title' : 'Je suis en manque', 'question' : 'Et toi, c’est quoi ta dope ?', 'hashtag' : '#pfouahQ1'},
+		    {'title' : 'Le rencard immaculé', 'question' : 'Deux jours sans douche, ça te ferait quoi ?', 'hashtag' : '#pfouahQ2'},
+		    {'title' : 'La bombe !', 'question' : 'Et toi, tu récupères quoi dans la mer ?', 'hashtag' : '#pfouahQ3'}
 		 ];
 
 		return{
